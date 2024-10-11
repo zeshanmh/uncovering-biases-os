2725f17d059b71aced23d30526546da7f3af92c9
zeshanmh <zeshanmh3@gmail.com>
2024-10-11 15:45:48 -0400

    adding berksons bias

--- synthetic/biases_synthetic.py
+++ synthetic/biases_synthetic.py
@@ -6,12 +6,12 @@ non-representative sample.
 import logging
 import numpy as np
 
-def apply_selection_bias(data, selection_dependence='Y'):
+def apply_selection_bias(data, selection_dependence='Y', beta_S_A=0.6, beta_S_Y=0.6):
     """
     Applies selection bias by selecting samples based on specified dependence.
     
     Parameters:
-    - data: DataFrame, observational study data
+    - data: DataFrame, observational study da   ta
     - selection_dependence: str, 'Y', 'A', or 'both' 
         - 'Y': selection probability increases with higher Y values
         - 'A': selection probability increases with higher A values
@@ -29,12 +29,35 @@ def apply_selection_bias(data, selection_dependence='Y'):
     elif selection_dependence == 'both':
         # Dependence on both A and Y
         selection_prob = 0.5 * data['A'] + 0.5 * (data['Y'] > data['Y'].median())
+    elif selection_dependence == 'berksons': 
+        linear_S = beta_S_A * data['A'] + beta_S_Y * data['Y']
+        selection_prob = 1 / (1 + np.exp(-linear_S))
     else:
         raise ValueError("Unsupported selection_dependence. Choose 'Y', 'A', or 'both'.")
 
     S = np.random.binomial(1, selection_prob, len(data))
+    import pdb; pdb.set_trace()
     data_selection_bias = data[S == 1].copy()
+    data_selection_bias = data_selection_bias.sample(n=data.shape[0], \
+                                                     replace=True, \
+                                                     random_state=42)
     return data_selection_bias
+
+def apply_exposure_misclassification(data, misclassification_prob=0.1):
+    """
+    Applies misclassification to exposure A.
+
+    Parameters:
+    - data: DataFrame, observational study data
+    - misclassification_prob: float, probability to flip A
+
+    Returns:
+    - DataFrame with misclassified A
+    """
+    A_misclassified = data['A'].apply(lambda x: 1 - x if np.random.rand() < misclassification_prob else x)
+    data_info_bias_A = data.copy()
+    data_info_bias_A['A'] = A_misclassified
+    return data_info_bias_A
 
 def apply_bias_wrapper(data, bias_dict):
     """
@@ -54,6 +77,9 @@ def apply_bias_wrapper(data, bias_dict):
     if bias_type == "selection_bias":
         logging.info("Bias applied successfully")
         return apply_selection_bias(data, bias_dict["params"]["selection_dependence"])
+    elif bias_type == "misclassification_bias":
+        logging.info("Bias applied successfully")
+        return apply_exposure_misclassification(data, bias_dict["params"]["misclassification_prob"])
     else:
         logging.warning(f"No bias applied")
         return data

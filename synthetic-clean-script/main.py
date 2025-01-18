7a8291b3fe0a139c10a00d478bdbe09e425fb412
Ilker Demirel <demirel@hera.csail.mit.edu>
2025-01-18 13:28:23 -0500

    final script + some results

--- synthetic-clean-script/main.py
+++ synthetic-clean-script/main.py
@@ -10,10 +10,10 @@ from utils_estimate import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--d', nargs="+", default=6, type=int)
-parser.add_argument('--n_val', default=100, type=int)
-parser.add_argument('--n_rct', default=2000, type=int)
-parser.add_argument('--n_obs', default=2000, type=int)
-parser.add_argument('--num_trials', default=5, type=int)
+parser.add_argument('--n_val', default=1000, type=int)
+parser.add_argument('--n_rct', default=1000, type=int)
+parser.add_argument('--n_obs', default=50000, type=int)
+parser.add_argument('--num_trials', default=200, type=int)
 parser.add_argument('--seed', default=99, type=int)
 parser.add_argument('--bias_S', action="store_true")
 parser.add_argument('--bias_A', action="store_true")
@@ -25,11 +25,7 @@ parser.add_argument('--bias_type', default="dummy_results", type=str)
 args = parser.parse_args()
 
 np.random.seed(args.seed)
-
-ds = args.d
-
-n_rct, n_rct_val = args.n_rct, args.n_val * 20
-n_obs, n_obs_val = args.n_obs, args.n_val * 20
+ds = args.d if isinstance(args.d, list) else [args.d]
 
 pl_range = (0.1, 0.2)
 ph_range = (0.8, 0.9)
@@ -37,36 +33,27 @@ ph_range = (0.8, 0.9)
 pX = {"R=0": [0.4, 0.6], "R=1": [0.6, 0.4]}
 bias_values = {"Y0": args.bias_Y0, "Y1": args.bias_Y1, "A": args.bias_A, "S": args.bias_S}
 
-res_keys = ["SE_Y0", "SE_Y1", "SE_A", "SE_S", "SE_R"]
+res_keys = ["SE_Y0", "SE_Y1", "SE_A", "SE_S"]
 
 for d in ds:
-    if args.full_adj:
-        d_meas = d
-    else:
-        d_meas = d - 1
-
+    d_meas = d if args.full_adj else d - 1
     predictors = [f"Xp{i + 1}" for i in range(2 ** d_meas)]
     cov_res = pd.DataFrame(columns=[key + suffix for key in res_keys for suffix in ["_r", "_p"]], index=np.arange(args.num_trials))
 
     for k in range(args.num_trials):
-        if args.bias_trs:
-            p_os_u0, p_rct_u0 = np.random.uniform(0.1,0.4), np.random.uniform(0.6,0.9)
-            pU = {"R=0": [p_os_u0, 1 - p_os_u0], "R=1": [p_rct_u0, 1 - p_rct_u0]}
-        else:
-            pU = pX
-
         obs_probs = sample_all_probs(d, pl_range, ph_range, bias_values)
         rct_probs = {"Y0": obs_probs["Y0"], "Y1": obs_probs["Y1"],  "A": (2 ** d) * [0.5], "S": (2 ** d) * [0.95]}
 
-        df_rct     = init_df(n_rct, d, d_meas, 1, rct_probs, pX, pU)
-        df_obs     = init_df(n_obs, d, d_meas, 0, obs_probs, pX, pU)
-        df_rct_val = init_df(n_rct_val, d, d_meas, 1, rct_probs, pX, pU)
-        df_obs_val = init_df(n_obs_val, d, d_meas, 0, obs_probs, pX, pU)
+        df_rct = init_df(args.n_rct, d, d_meas, 1, rct_probs, pX, False)
+        df_obs = init_df(args.n_obs, d, d_meas, 0, obs_probs, pX, args.bias_trs)
 
         rct_models = fit_models(df_rct, predictors)
-        make_preds(df_rct_val, predictors, rct_models)
-        
         obs_models = fit_models(df_obs, predictors)
+
+        df_rct_val = init_df(args.n_val * 20, d, d_meas, 1, rct_probs, pX, False)
+        df_obs_val = init_df(args.n_val * 20, d, d_meas, 0, obs_probs, pX, args.bias_trs)
+
+        make_preds(df_rct_val, predictors, rct_models)
         make_preds(df_obs_val, predictors, obs_models)
 
         df_val = merge_df_val(df_rct_val, df_obs_val, predictors, rct_models, obs_models)
@@ -76,10 +63,10 @@ for d in ds:
             cov_res.loc[k, key + "_r"] = r
             cov_res.loc[k, key + "_p"] = p
 
-    save_dir = Path(os.path.dirname(os.path.abspath(__file__)) + f"/results/d{d}/{args.bias_type}")
+    save_dir = Path(os.path.dirname(os.path.abspath(__file__)) + f"/results/{args.bias_type}/d{d}")
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    cov_res.to_csv(os.path.join(save_dir, 'r_p.csv'), index=False)    
+    cov_res.to_csv(os.path.join(save_dir, 'results.csv'), index=False)    
 
     with open(os.path.join(save_dir, 'args.txt'), 'w') as f:
         json.dump(args.__dict__, f, indent=2)
--- synthetic-clean-script/utils_data_gen.py
+++ synthetic-clean-script/utils_data_gen.py
@@ -67,27 +67,26 @@ def sample_all_probs(d, pl_range, ph_range, scenario):
 
 
 def init_df(n, d, d_meas, r, probs, x_probs, trs_bias):
-    covs = [f"X{i + 1}" for i in range(d)]
+    all_covs = [f"X{i + 1}" for i in range(d)]
+    X_covs = [f"X{i + 1}" for i in range(d - 1)]
     meas_covs = [f"X{i + 1}" for i in range(d_meas)]
     
-    X_meas = choice([0, 1], size=(n, d_meas), p=x_probs[f"R={r}"])
+    X = choice([0, 1], size=(n, d - 1), p=x_probs[f"R={r}"])
+    df = pd.DataFrame({**{'R': r}, **{cov: X[:,i] for i, cov in enumerate(X_covs)}})
+
+    if not trs_bias:
+        df[f"X{d}"] = choice([0, 1], size=(n, 1), p=[0.5, 0.5])
+    else:
+        u_prob = sample_probs(d - 1, (0.1, 0.9), (0.1, 0.9), True)
+        df[f"P(X{d}=1)"] = df.apply(lambda row: covs_to_prob(row, X_covs, u_prob), axis=1)
+        df[f"X{d}"] = df.apply(lambda row: binomial(1, row[f"P(X{d}=1)"]), axis=1)
+
+    for key, prob in probs.items():
+        df[f"P({key}=1)"] = df.apply(lambda row: covs_to_prob(row, all_covs, prob), axis=1)
+        df[key] = df.apply(lambda row: binomial(1, row[f"P({key}=1)"]), axis=1)
 
     for i, c in enumerate(list(itertools.product([0, 1], repeat=d_meas))):
         df[f"Xp{i + 1}"] = (df[meas_covs] == c).all(axis=1).astype(int)
-
-    if trs_bias:
-        u_prob = sample_probs(d_meas, (0.2, 0.8), (0.2, 0.8), True)
-        df["P(U=1)"] = df.apply(lambda row: covs_to_prob(row, covs, u_prob), axis=1)
-        df["U"] = df.apply(lambda row: binomial(1, row["P(U=1)"]), axis=1)
-    else:
-        U = choice([0, 1], size=(n,1), p=x_probs[f"R={r}"])
-
-    X = np.concatenate((X_meas, U), axis=1)
-    df = pd.DataFrame({**{'R': r}, **{cov: X[:,i] for i, cov in enumerate(covs)}})
-
-    for key, prob in probs.items():
-        df[f"P({key}=1)"] = df.apply(lambda row: covs_to_prob(row, covs, prob), axis=1)
-        df[key] = df.apply(lambda row: binomial(1, row[f"P({key}=1)"]), axis=1)
 
     df["Y"] = df["A"] * df["Y1"] + (1 - df["A"]) * df["Y0"]   
 

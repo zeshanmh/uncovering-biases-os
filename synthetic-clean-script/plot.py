90b6b51ba7092f832126e559fc98d1fd92f7bb13
zeshanmh <zeshanmh3@gmail.com>
2025-01-29 13:49:24 -0500

    plotting for selection bias type 2

--- synthetic-clean-script/plot.py
+++ synthetic-clean-script/plot.py
@@ -18,7 +18,7 @@ target_vars = {"SE_S": "S", "SE_A": "A", "SE_Y1": "Y"}
 
 pval = 0.01
 
-sb_type2_flag = "selection_bias_type2" in args.bias_type
+sb_type2_flag = "sel_bias_type2" in args.bias_type
 
 for d in ds:
     save_dir = Path(os.path.dirname(os.path.abspath(__file__)) + f"/results_ntrain-{args.n_rct}_nval-{args.n_val}/{args.bias_type}/d{d}")
@@ -27,13 +27,13 @@ for d in ds:
     # cp = sns.color_palette("tab10")
     cp = ("#0f7ba2", "#dd5129", "#43b284")
 
-    fig, axs = plt.subplots(1, 3, figsize=(10,2.5)) 
+    fig, axs = plt.subplots(1, 3, figsize=(6,1.5)) 
     
     x_min, x_max = float('inf'), float('-inf')
     y_min, y_max = float('inf'), float('-inf')
 
     for idx, (key, plot_name) in enumerate(target_vars.items()):
-        axs[idx].set_xlabel(rf"$\rho (b1, {plot_name})$", fontsize=10)
+        # axs[idx].set_xlabel(rf"$\rho (b1, {plot_name})$", fontsize=10)
         axs[idx].axhline(y=-np.log10(pval), color='lightgray', linestyle='--', label=f'p = {pval}')
         axs[idx].axvline(x=0, color='lightgray', linestyle='--', label=f'p = {pval}')
         
@@ -55,7 +55,7 @@ for d in ds:
         x_min, x_max = min(x_min, x0), max(x_max, x1)
         y_min, y_max = min(y_min, y0), max(y_max, y1)
 
-    axs[0].set_ylabel('-log10(p-value)', fontsize=10)
+    # axs[0].set_ylabel('-log10(p-value)', fontsize=10)
 
     for i,ax in enumerate(axs.flat):
         ax.set_xlim(x_min, x_max)
@@ -75,7 +75,7 @@ for d in ds:
         
 
     plt.tight_layout()
-    plt.savefig(os.path.join(save_dir, 'plot.png'))
+    plt.savefig(os.path.join(save_dir, 'plot.svg'))
 
     #############################################################################################
 
